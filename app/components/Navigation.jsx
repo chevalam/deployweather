46608304f68bd41645a64cf729174d9ab1323e64
Srikanth Chevalam <srikanth.chevalam@gmail.com>
2017-08-24 22:48:45 +0530

    Navigation component styled using foundations library.
    top-bar/top-bar-left/top-bar-right from foundations library
    are styled in this patch (foundation.zurb.com/sites/docs/top-bar.html)
    can be referred for more examples and references.
    
    menu/menu-text is used to style menu horizantally .
    
    Signed-off-by: Srikanth Chevalam <srikanth.chevalam@gmail.com>

--- app/components/Navigation.jsx
+++ app/components/Navigation.jsx
@@ -2,26 +2,52 @@ var React = require('react');
 var About = require('About');
 var {Link,IndexLink} = require('react-router');
 
-// var Navigation = React.createClass({
-//   render: function(){
-//     return (
-//       <div>
-//       <h1>Navigation Component</h1>
-//       <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight:'bold'}}>Get Weather </IndexLink>
-//       <Link to="/about" activeClassName="active" activeStyle={{fontWeight:'bold'}}>  About</Link>
-//       <Link to="/examples" activeClassName="active" activeStyle={{fontWeight:'bold'}}> Examples</Link>
-//       </div>
-//     );
-//   }
-// });
-var Navigation = (props) => {
-  return (
-    <div>
-    <h1>Navigation Component</h1>
-    <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight:'bold'}}>Get Weather </IndexLink>
-    <Link to="/about" activeClassName="active" activeStyle={{fontWeight:'bold'}}>  About</Link>
-    <Link to="/examples" activeClassName="active" activeStyle={{fontWeight:'bold'}}> Examples</Link>
-    </div>
-  )
-}
+var Navigation = React.createClass({
+  onSearch: function(e){
+    e.preventDefault();
+    alert("Yet to wire stuff!! Folks at work");    
+  },
+  render: function(){
+    return (
+      <div className="top-bar">
+        <div className="top-bar-left">
+          <ul className="menu">
+            <li className="menu-text">React Weather App</li>
+            <li>
+              <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight:'bold'}}>Get Weather </IndexLink>
+            </li>
+            <li>
+              <Link to="/about" activeClassName="active" activeStyle={{fontWeight:'bold'}}>  About</Link>
+            </li>
+            <li>
+              <Link to="/examples" activeClassName="active" activeStyle={{fontWeight:'bold'}}> Examples</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="top-bar-right">
+          <form onSubmit={this.onSearch}>
+          <ul className="menu">
+            <li>
+              <input type="text" placeholder="Enter city"></input>
+            </li>
+            <li>
+              <input type="submit" className="button" value="Get Weather"></input>
+            </li>
+          </ul>
+          </form>
+        </div>
+      </div>
+    );
+  }
+});
+// var Navigation = (props) => {
+//   return (
+//     <div>
+//     <h1>Navigation Component</h1>
+//     <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight:'bold'}}>Get Weather </IndexLink>
+//     <Link to="/about" activeClassName="active" activeStyle={{fontWeight:'bold'}}>  About</Link>
+//     <Link to="/examples" activeClassName="active" activeStyle={{fontWeight:'bold'}}> Examples</Link>
+//     </div>
+//   )
+// }
 module.exports = Navigation;
