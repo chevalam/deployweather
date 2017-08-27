3b48f74502d56d06fa2aff0455709447446debc1
Srikanth Chevalam <srikanth.chevalam@gmail.com>
2017-08-27 20:17:05 +0530

    About page styled using foundation libraries
    Signed-off-by: Srikanth Chevalam <srikanth.chevalam@gmail.com>

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -9,8 +9,22 @@ var React = require('react');
 
 var About = (props) => {
   return (<div>
-          <h3>About</h3>
-          <p>The Author is a fly feeding on fleece and can be contacted on any job sites </p>
+          <h1 className="text-center">About</h1>
+          <p>The Author is a fly feeding on fleece and can be contacted on any job sites.
+          This is a weather application built with react styled with foundation library.
+          Credits to foundation guys. </p>
+        <p>
+          Some tools that are used below :
+        </p>
+        <ul>
+          <li>
+            <a href="https://facebook.github.io/react"> React</a> - Javascript framework used
+          </li>
+          <li>
+            <a href="http://openweathermap.org"> OpenWeatherMap</a> - apis exposed for OpenWeatherMap
+              to retrieve weather information based on city
+          </li>
+        </ul>
           </div>)
 }
 module.exports = About;
--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -5,9 +5,11 @@ var Main = (props) => {
   return (
       <div>
         <Navigation />
-      <div className="coloumns large-4 medium-6 small-centered">
+        <div className="row">
+      <div className="columns large-4 medium-6 small-centered">
         {props.children}
       </div>
+    </div>
     </div>
   )
 }
